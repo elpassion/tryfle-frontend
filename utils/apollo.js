b90d06388d097dde1e98c03181c30c46fa5b4510
Urszula Kowalska <ua.kowalska@gmail.com>
2020-08-28 12:15:24 +0200

    Add material-ui blog layout

---
+++ utils/apollo.js
@@ -0,0 +1,19 @@
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import withApollo from "next-with-apollo";
+import { createHttpLink } from "apollo-link-http";
+import fetch from "isomorphic-unfetch";
+
+const link = createHttpLink({
+  fetch,
+  uri: "http://localhost:1337/graphql"
+});
+
+export default withApollo(
+  ({ initialState }) =>
+    new ApolloClient({
+      link: link,
+      cache: new InMemoryCache()
+        .restore(initialState || {})
+    })
+);
